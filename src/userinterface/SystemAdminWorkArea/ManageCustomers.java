8fa0a1af2594a65a90182f66b8f9ac38d467b0c8
Bhavana Yalam <yalam.b@northeastern.edu>
2021-11-21 22:14:24 -0500

    Assignment 5

--- src/userinterface/SystemAdminWorkArea/ManageCustomers.java
+++ src/userinterface/SystemAdminWorkArea/ManageCustomers.java
@@ -283,7 +283,7 @@ public class ManageCustomers extends javax.swing.JPanel {
             nameJTextField.setText(user.getName()+"");
             uNameTextField.setText(user.getUsername()+"");
             PasswordField.setText(user.getPassword()+"");
-            // system.getUserAccountDirectory().deleteUserAccount(user);
+          
 
         }
         else {
